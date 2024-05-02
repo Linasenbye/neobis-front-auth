75e563fa8e9995ac148b4f7b2b1e5e5a7f4c5f26
Linasenbye <elyneseh@gmail.com>
2024-05-02 21:07:32 +0900

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {useForm} from 'react-hook-form'
+import lorby from '../images/illustration.png'
+
+const Home = () => {
+    const {
+        register, 
+        formState: {
+            errors, 
+        }, 
+        handleSubmit,
+
+    } = useForm();
+
+    const onSubmit = (data) => {
+        alert(JSON.stringify(data))
+    }
+
+    return (
+        <section className='login-page'>
+            <div className='left-side'>
+                <img src={lorby} alt='image'/>
+                <h1>Lorby</h1>
+                <p>Твой личный репетитор</p>
+            </div>
+            <form className = "right-side" onSubmit={handleSubmit(onSubmit)}>
+                <h1>Вэлком бэк!</h1>
+                <label>
+                    <input type="text" placeholder='Введи туда-сюда логин' {...register('firstName', {required: "Неверный логин или пароль",})}/>
+                    <input type="text" placeholder='Пароль (тоже введи)' {...register('firstName', {required: "Неверный логин или пароль",})}/>
+                </label>
+                <div>
+                    {errors?.firstName && <p>{errors?.firstName?.message || "Error!"}</p>}
+                </div>
+                <input type="submit" value="Войти"/>
+                <p className='register'>У меня еще нет аккаунта</p>
+            </form>
+        </section>
+    );
+};
+
+export default Home;
